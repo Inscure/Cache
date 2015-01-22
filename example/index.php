77fde8d26da4a18d76d867cafb6ba4b1ce02cb17
Inscure <drimer.eco@gmail.com>
2015-01-23 00:23:53 +0100

    -

--- example/index.php
+++ example/index.php
@@ -1,18 +1,3 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$array = new Collection\Collection(array(1, 2, 3, 4, 5));
-
-foreach($array as $key => $value) {
-    echo $key . '=>' . $value . '<br />';
-}
-
-echo '<br />';
-
-$array[0] = null;
-
-foreach($array as $key => $value) {
-    echo $key . '=>' . $value . '<br />';
-}
-
